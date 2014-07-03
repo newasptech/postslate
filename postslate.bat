8d8c050cb20c7eb984926cb1c9a0867f31d056cd
Mark Echeverri <ps14@newasptech.com>
2014-07-03 15:25:50 -0700

    Add 32/64-bit JVM check to Windows batch file

--- postslate.bat
+++ postslate.bat
@@ -1,3 +1,12 @@
-@echo on
-java -Xmx1000M -jar postslate.jar %*
+java -version 2>&1 | find "64-Bit" >nul:
 
+if errorlevel 1 (
+    rem 32-bit
+    set mem=1000
+) else (
+    rem 64-bit
+    set mem=2000
+)
+
+@echo on
+java -Xmx%mem%M -jar postslate.jar %*
